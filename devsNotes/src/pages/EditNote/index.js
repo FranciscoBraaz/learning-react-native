3340d8213d47be15eb1aad65989851f8d82d0343
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-01-05 19:21:46 -0300

    estilizando tela de edição de anotação

--- devsNotes/src/pages/EditNote/index.js
+++ devsNotes/src/pages/EditNote/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Container, Title} from './styles';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Container, TitleInput, BodyInput} from './styles';
 
 export function EditNote() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const list = useSelector(state => state.notes.list);
+  const [status, setStatus] = useState('new');
+
+  useEffect(() => {
+    if (route.params?.key !== undefined && list[route.params.key]) {
+      setStatus('edit');
+      setTitle(list[route.params.key].title);
+      setDescription(list[route.params.key].description);
+    }
+  }, []);
+
   return (
     <Container>
-      <Title>Editar nota</Title>
+      <TitleInput
+        value={title}
+        onChangeText={event => setTitle(event)}
+        placeholderTextColor="#CCC"
+        placeholder="Digite o título da anotação"
+        autoFocus={true}
+      />
+      <BodyInput
+        value={description}
+        onChangeText={event => setDescription(event)}
+        placeholder="Digite a descrição da anotação"
+        multiline={true}
+        placeholderTextColor="#CCC"
+        textAlignVertical="top"
+      />
     </Container>
   );
 }
--- devsNotes/src/pages/EditNote/styles.js
+++ devsNotes/src/pages/EditNote/styles.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: #333;
 `;
 
-export const Title = styled.Text`
-  font-size: 16px;
+export const TitleInput = styled.TextInput`
+  font-size: 20px;
+  color: #fff;
+  padding: 15px;
+  font-weight: bold;
+  border-style: solid;
+  border-color: #222;
+  border-bottom-width: 1px;
+`;
+
+export const BodyInput = styled.TextInput`
+  font-size: 20px;
+  color: #fff;
+  padding: 15px;
+  flex: 1;
 `;
